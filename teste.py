eca290c9025b2c593663c9e3b7e704fce3e854b6
Vini-Assuncao <vinicius.a.santos24@aluno.senai.br>
2025-04-01 13:33:46 -0300

    Primeiro commit branch raiz
    Estamos testando o uso de branch

--- teste.py
+++ teste.py
@@ -1,1 +1,2 @@
-print("Estamos testando a criação do arquivo. Estte é o arquivo principal")
+print("Estamos testando a criação do arquivo. Este é o arquivo principal")
+print("Agora estou na branch raiz1 e não no arquivo principal") 
